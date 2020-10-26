9df3d9bd29ab711da0c817edf2a946819c06dd47
Siddharth <snasa045@uottawa.ca>
2020-10-26 00:12:51 -0400

    The single data observable pattern

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -2,12 +2,32 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { Course } from "../model/course";
-import { map, shareReplay } from "rxjs/operators";
+import { map, shareReplay, tap } from "rxjs/operators";
 import { Lesson } from "../model/lesson";
 
 @Injectable({ providedIn: "root" })
 export class CoursesService {
   constructor(private http: HttpClient) {}
+
+  loadCourseById(courseId: number): Observable<Course> {
+    return this.http
+      .get<Course>(`/api/courses/${courseId}`)
+      .pipe(shareReplay());
+  }
+
+  loadAllCourseLessons(courseId: number): Observable<Lesson[]> {
+    return this.http
+      .get<Lesson[]>("/api/lessons", {
+        params: {
+          pageSize: "100000",
+          courseId: courseId.toString(),
+        },
+      })
+      .pipe(
+        map((res) => res["payload"]),
+        shareReplay()
+      );
+  }
 
   loadAllCourses(): Observable<Course[]> {
     return this.http.get<Course[]>("/api/courses").pipe(
