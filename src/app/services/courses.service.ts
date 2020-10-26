925adffa4fa416afb0c045c827c22ec718659558
Siddharth Nasare <snasa045@uottawa.ca>
2020-10-25 22:48:33 -0400

    Start (#4)
    * Stateless Observables services
    
    * Reactive Component Interaction : Courses feature
    
    * Reactive Component Interaction : Courses feature
    
    * Authentication State Management
    
    * Authentication State Management
    
    * Master-detail UI pattern

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { Course } from "../model/course";
 import { map, shareReplay } from "rxjs/operators";
+import { Lesson } from "../model/lesson";
 
 @Injectable({ providedIn: "root" })
 export class CoursesService {
@@ -19,5 +20,19 @@ export class CoursesService {
     return this.http
       .put(`/api/courses/${courseId}`, changes)
       .pipe(shareReplay());
+  }
+
+  searchLessons(search: string): Observable<Lesson[]> {
+    return this.http
+      .get<Lesson[]>("/api/lessons", {
+        params: {
+          filter: search,
+          pageSize: "100",
+        },
+      })
+      .pipe(
+        map((res) => res["payload"]),
+        shareReplay()
+      );
   }
 }
